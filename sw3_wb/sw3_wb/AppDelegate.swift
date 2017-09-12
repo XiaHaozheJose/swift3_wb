23d48286f55006d962297343dd9ca17eb0c2088c
haozhe <haozhexia@hotmail.com>
2017-09-13 00:34:40 +0200

    窗口部署完毕

--- sw3_wb/sw3_wb/AppDelegate.swift
+++ sw3_wb/sw3_wb/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        window = UIWindow()
+        
+        window?.rootViewController = WB_MainViewController()
+        
+        window?.makeKeyAndVisible()
+        
+        
+        
+        
         return true
     }
 
---
+++ sw3_wb/sw3_wb/Classes/View/Home/WB_HomeViewController.swift
@@ -0,0 +1,37 @@
+
+
+//
+//  WB_HomeViewController.swift
+//  sw3_wb
+//
+//  Created by 浩哲 夏 on 2017/9/13.
+//  Copyright © 2017年 浩哲 夏. All rights reserved.
+//
+
+import UIKit
+
+class WB_HomeViewController: WB_BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
