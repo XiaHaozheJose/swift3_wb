05e3c2754a5a4784c4302e91a9809fe300b0aa53
haozhe <haozhexia@hotmail.com>
2017-09-14 23:12:30 +0200

    字典添加自控制器

--- sw3_wb/sw3_wb/Classes/View/Main/WB_MainViewController.swift
+++ sw3_wb/sw3_wb/Classes/View/Main/WB_MainViewController.swift
@@ -10,26 +10,50 @@ import UIKit
 
 class WB_MainViewController: UITabBarController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        setChildController()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+
+extension WB_MainViewController{
+    fileprivate func setChildController(){
+        let arrayDict = [["clsName":"WB_HomeViewController","title":"首页","imageName":""]]
+        
+        var arrayM = [UIViewController]()
+        
+        for dict in arrayDict{
+            arrayM.append(controller(dict: dict))
+        }
+        viewControllers = arrayM
     }
-    */
-
+    
+    fileprivate func controller(dict: [String: String])->UIViewController{
+        guard let clsName = dict["clsName"], let title = dict["title"], let imageName = dict["imageName"],
+            let cls = NSClassFromString(Bundle.main.namespace + "." + clsName) as? UIViewController.Type
+            else {
+            return UIViewController()
+        }
+        
+        let vc = cls.init()
+        
+        vc.title = title;
+        
+        let nav = WB_NavigationViewController(rootViewController: vc)
+        
+        return nav
+        
+        
+        
+        
+    }
 }
